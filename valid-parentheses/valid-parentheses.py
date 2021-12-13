8faec4fbca6e2009aae0adac72854b568c55b7ef
Kiana <kiana508@gmail.com>
2021-12-13 15:58:32 -0600

    Time: 28 ms (86.81%), Space: 14.3 MB (36.49%) - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -1,17 +1,26 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-
+        # edge case:
+        if not s or len(s) == 1:
+            return False
+        
+        
+        dic = {')': '(',
+               '}': '{',
+               ']': '['}
+        
+        # method: use stack/list
         stack = []
-        dic = { ')': '(',
-                ']': '[',
-                '}': '{'
-        }
+        
         for s1 in s:
-            if s1 in list(dic.values()):
+            # if (, [, {, append
+            if s1 not in dic:
                 stack.append(s1)
+            # else, pop end of list
             else:
                 if stack and stack[-1] == dic[s1]:
                     stack.pop()
                 else:
                     return False
+                
         return True if len(stack) == 0 else False
