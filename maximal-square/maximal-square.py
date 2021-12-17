4cffe2bcdd7628bbdb0b997e3aa3e6d7c1be3aff
Kiana <kiana508@gmail.com>
2021-12-16 22:04:41 -0600

    Time: 216 ms (54.41%), Space: 15.7 MB (54.93%) - LeetHub

---
+++ maximal-square/maximal-square.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        dp, ans = [[0] * len(matrix[0]) for row in matrix], 0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == '1':
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+                    ans = max(ans, dp[i][j] ** 2)
+                    
+        return ans
