3af7d6e32d5dd45caeab831f1659c749cdf91f9d
Kiana <kiana508@gmail.com>
2021-12-20 22:04:44 -0600

    Time: 48 ms (47.09%), Space: 13.7 MB (95.35%) - LeetHub

---
+++ word-ladder-ii/word-ladder-ii.py
@@ -0,0 +1,79 @@
+class Solution(object):
+    def findLadders(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: List[List[str]]
+        """
+        wordList = set(wordList)
+        # edge case
+        if endWord not in wordList:
+            return []
+        
+        wordList.add(beginWord)
+        wordList.add(endWord)
+        
+        # records of shortest path from biginWord for each word in wordList
+        distance = {}
+        
+        # records of from one word, all next word (with possible shortest path)
+        fromToDict = {}
+        
+        for s in wordList:
+            fromToDict[s] = []
+        self.bfs(beginWord, fromToDict, distance, wordList)
+        
+        # for shortest path
+        results = []
+        if endWord not in distance:
+            return []
+        self.dfs(beginWord, endWord, distance, wordList, [], results, fromToDict, distance[endWord])
+        
+        return results
+    
+    def bfs(self, start, fromToDict, distance, d):
+        distance[start] = 0
+        queue = deque([start])
+        
+        while queue:
+            curr_word = queue.popleft()
+            # for word in the wordList which are just one char diff with curr_word
+            for next_word in self.get_next_words(curr_word, d):
+                # if next_word not shown before or next_word distance like current path, there is a path from curr_word to next_word
+                if (next_word not in distance or distance[next_word] == distance[curr_word]+1):
+                    fromToDict[curr_word].append(next_word)
+                    
+                # if next_word not shown before, record from start, to every word, the shortest path, then find the word from nextWord
+                if next_word not in distance:
+                    distance[next_word] = distance[curr_word] + 1
+                    queue.append(next_word)
+                    
+    def dfs(self, curr_word, target, distance, d, path, results, fromToDict, min_len):
+        if len(path) == min_len + 1:
+            return
+
+        path.append(curr_word)
+
+        if curr_word == target:
+            results.append(list(path))
+        else:
+            for nextWord in fromToDict[curr_word]:
+                self.dfs(nextWord, target, distance, d, path, results, fromToDict, min_len)
+
+        path.pop()
+            
+    # find all words in the wordList/d which are just one char diff with word
+    def get_next_words(self, word, d):
+        words = []
+        for i in range(len(word)):
+            for c in string.ascii_lowercase:
+                if (word[i] == c):
+                    continue;
+                next_word = word[:i] + c + word[i+1:]
+                if next_word in d:
+                    words.append(next_word)
+
+        return words
+                
+        
