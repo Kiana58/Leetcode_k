1b65eab636584d991bbb24795d0a950cd764addf
Kiana <kiana508@gmail.com>
2021-12-28 22:44:53 -0600

    Time: 76 ms (47.11%), Space: 29.7 MB (5.04%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -1,40 +1,33 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-        # if empty
-        if not root:
-            return None
-        
-        if root == p or root == q:
-            return root
-        
-        # find in left or right subtree
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-        
-        # if find one in left subtree, the other in right subtree
-        if left and right:
-            return root
-        
-        # if both in left subtree
-        if left:
-            return left
-        
-        # if both in right subtree
-        if right:
-            return right
-        
-        # if neither in left or right
-        return None
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # method: if both in left or right tree, then return p or q; if not, return root
+        # #######################
+        # High-Level Approach #
+        #######################
+        # If both nodes are in the tree, vanilla search for both nodes eventually succeeds.
+
+        def recursive_dfs(root, n1, n2, result):
+            # edge case
+            if not root:
+                return False, False # n1_found, n2_found
+            n1_eq_curr, n2_eq_curr = n1 == root, n2 == root
+            
+            n1_in_l, n2_in_l = recursive_dfs(root.left, n1, n2, result)
+            n1_in_r, n2_in_r = recursive_dfs(root.right, n1, n2, result)
+            
+            n1_found = n1_eq_curr or n1_in_l or n1_in_r
+            n2_found = n2_eq_curr or n2_in_l or n2_in_r
+            both_found = n1_found and n2_found
+            if both_found:
+                result.append(root)
+            return n1_found, n2_found
+        result = []
+        recursive_dfs(root, p, q, result)
+        return result[0] if result else None # handles p or q not in root
