53ae5843df0677ccd1db6c3f593c76d657d05a3e
Kiana <kiana508@gmail.com>
2021-12-30 14:13:05 -0600

    Time: 1717 ms (34.81%), Space: 14.2 MB (93.67%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.py
+++ longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,26 +1,42 @@
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        # base case, empty or not
-        if not s:
-            return s
-        # initiate answer: (length of Palindromic, start position) which could transfer to (index start, index end) in s, put length first, because need max...
-        answer = (0, 0)
-        for mid in range(len(s)):
-            # if Palindromeic len is odd
-            answer = max(answer, self.get_Palindrome_from_mid(s, mid, mid))
-            # if Palindromeic len is even
-            answer = max(answer, self.get_Palindrome_from_mid(s, mid, mid+1))
-            
-        return s[answer[1]: answer[1]+answer[0]]
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # method: DP
+#         length = len(s)
+#         dp = {}
+#         max_len = 1
+#         start = 0
         
-    def get_Palindrome_from_mid(self, s, left, right):
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            # from mid to both sides!!!
-            left -= 1
-            right += 1
-        # length of Palindromic, start position
-        return right - left - 1, left +1
+#         for _len in range(2, length+1):
+#             i = 0
+#             while i <= length - _len:
+#                 j = i + _len - 1
+#                 # base case: P(i, t) = true, P(i, i+1) = (Si == Si+1)
+#                 # logic: P(i, i) = true, P(i,j)=(P(i+1,j−1) and Si==Sj)
+#                 if (_len <= 3 and s[i] == s[j]) or (s[i] == s[j] and dp.get((i + 1, j - 1))):
+#                     dp[(i, j)] = True
+#                     if _len > max_len:
+#                         max_len = _len
+#                         start = i
+#                 i += 1
+#         return s[start:start + max_len]
+        # method: expand from middle
+        self.ans = ""
+        self.ansLen = 0
+        n = len(s)
+        
+        def expand(l, r):
+            while l >= 0 and r < n and s[l] == s[r]:
+                if r-l+1 > self.ansLen:
+                    self.ansLen = r-l+1
+                    self.ans = s[l:r+1]
+                l -= 1 
+                r += 1
+                    
+        for i in range(n):
+            # Odd Palindrom Strings
+            expand(i, i)
+                    
+            # Even Palindrom Strings
+            expand(i, i+1)
+            
+        return self.ans
